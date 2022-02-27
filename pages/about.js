f4fc6e5efa9acfd7c9d7629b7276d6a67406077a
Neha <nehaprajapati1150@gmail.com>
2022-02-28 01:14:04 +0530

    navigating between pages

--- pages/about.js
+++ pages/about.js
@@ -1,2 +1,28 @@
-const AboutPage=()=><p>About Page</p>
-export default AboutPage
+import { withRouter } from "next/router"
+
+const AboutPage=(props)=>{
+    const router=useRouter()
+    return (
+        <div>
+            <p>About Page</p>
+            <button onClick={()=>props.router.push("/")}>Go To Home</button>
+        </div>
+    )
+}
+export default withRouter(AboutPage)
+
+
+//OR------------------
+
+import { useRouter } from "next/router"
+
+// const AboutPage=()=>{
+//     const router=useRouter()
+//     return (
+//         <div>
+//             <p>About Page</p>
+//             <button onClick={()=>router.push("/")}>Go To Home</button>
+//         </div>
+//     )
+// }
+// export default AboutPage
